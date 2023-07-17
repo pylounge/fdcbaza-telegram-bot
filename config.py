5e63f2db030cc9e1122857fb3104198f1a712e83
Макс <78209508+pylounge@users.noreply.github.com>
2023-07-17 17:39:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 settings = {
-    'TOKEN':'111',
-    'LOG_FILE': 'poll-bot.log'
+    'TOKEN': os.getenv('TELEGRAM_TOKEN'),
+    'LOG_FILE': os.getenv('LOG_FILE_PATH'),
+    'DOC_URL': os.getenv('DOC_URL')
 }
